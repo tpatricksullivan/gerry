4317bb673319444b0a43530cf6a738c33bb234fa
Thomas Patrick Sullivan <tpatricksullivan@midway-login1.rcc.local>
2017-11-28 21:57:33 -0600

    fixed degerry function to allow districts with one block
    Signed-off-by: Thomas Patrick Sullivan <tpatricksullivan@midway-login1.rcc.local>

---
+++ degerry.jl
@@ -0,0 +1,102 @@
+using JuMP, Gurobi
+function degerry(
+    votes,
+    contiguity_matrix, 
+    number_districts,
+    common_size_threshold = 0.2
+    )
+    
+    _V = votes
+    _C = contiguity_matrix
+
+    blocks = size(_V,1)
+    districts = number_districts
+    total_vote = _V * ones(2,1)
+
+    # Do some checks
+    if any(_C != _C')
+        throw(ArgumentError("Contiguity matrix is not valid. It must be symmetric."))
+    end
+    
+    #m = Model(solver = CbcSolver())
+    m = Model(solver = GurobiSolver(Presolve=0))
+    
+    ## Variables
+
+    @variable(m, 0 <= D[i=1:blocks,j=1:districts] <= 1 , Bin)
+    
+    ## Constraints  
+
+    # each block can be in only one district
+    @constraint(m, D * ones(districts,1) .== 1)  
+    
+    # Each district must have at least one block
+    # @constraint(m, (D' * V) * [1;1] .>= 1)
+
+    # These constraints set wasted_u to the number of wasted votes for the losing party
+    @variable(m, 0 <= w[i=1:districts] <= 1, Bin)
+    @variable(m, wasted_u[i=1:districts, j=1:2])
+    M = blocks * sum(total_vote) 
+    @constraint(m, wasted_u .>= 0)
+    @constraint(m, wasted_u[:,1] .>= (D' * _V)[:,1] - M * w)
+    @constraint(m, wasted_u[:,2] .>= (D' * _V)[:,2] - M * (1-w))
+
+    # These constraints set wasted_o to the number of wasted votes for the winning party
+    @variable(m, wasted_o[i=1:districts, j=1:2])
+    @variable(m, votes_to_win[i=1:districts])
+    @constraint(m, votes_to_win .== (D' * _V) * [1;1] / 2)
+    @constraint(m, wasted_o .>= 0)
+    @constraint(m, wasted_o .>= (D' * _V) - votes_to_win * [1 1])
+
+    # These constraints calculate the efficiency gap
+    @variable(m, eff_gap)
+    @variable(m, abs_eff_gap)
+    @constraint(m, eff_gap .== ones(1,districts) * (wasted_u + wasted_o) * [1;-1])
+    @constraint(m, abs_eff_gap >= eff_gap)
+    @constraint(m, abs_eff_gap >= - eff_gap)
+
+    # These constraints enforce roughly equal sizes. 
+    @variable(m, common_size) # this approach is too slow
+    fixed_common_size = sum(_V) / districts
+    # @constraint(m, (D' * _V) * [1;1] .>= fixed_common_size * (1-common_size_threshold))
+    @constraint(m, (D' * _V) * [1;1] .<= fixed_common_size * (1+common_size_threshold))
+
+    # These constraints enforce contiguity
+    @variable(m, 0 <= multi_block_districts[i=1:districts] <= 1, Bin)
+    @constraint(m, multi_block_districts .>= 0 )
+    @constraint(m, M * multi_block_districts' .>= ones(1,blocks) * D - ones(1,districts) )
+    # @constraint(m, _C * D .>= 2 * D) this by itself is not enough
+    @constraint(m, _C * D .>= 2 * D - M * (1-repmat(multi_block_districts',blocks)))
+    
+
+    ## Objective
+
+    @objective(m, Min, abs_eff_gap  + sum(wasted_u) + sum(wasted_o) + sum(multi_block_districts) ) 
+    
+    @time begin
+        status = solve(m)
+    end
+    
+    res = Dict([("Model",m),
+        ("Solve Status", status), 
+        ("Efficiency Gap", getvalue(abs_eff_gap) ),
+        ("Wasted Over Votes", getvalue(wasted_o)),
+        ("Wasted Under Votes", getvalue(wasted_u)),
+        ("Total Wasted Votes [D R]", ones(1,districts) * ( getvalue(wasted_u) + getvalue(wasted_o))),
+        ("Votes By District", getvalue(D)' * _V), 
+        ("Common Size", getvalue(common_size)), 
+        ("Fixed Common Size", fixed_common_size), 
+        ("District Assignments", getvalue(D))
+    ])
+    
+    return res
+end
+
+
+# Load data
+using CSV
+WI_votes = CSV.read("data/Gerrymander County_election_data.csv")
+WI_contiguity = CSV.read("data/Gerrymander County_contiguity.csv", rows = 73)
+
+WI_V = convert(Array, WI_votes[:,3:4])
+WI_C = convert(Array, WI_contiguity[:,2:73])
